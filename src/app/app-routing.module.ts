590e54410f28fe36ac44df64e818a6cb78583777
ddeluc <56974496+ddeluc@users.noreply.github.com>
2020-12-05 03:13:01 -0500

    Guard Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup';
 import { ScheduleCreateComponent } from './schedules/schedule-create/schedule-create.component';
@@ -8,14 +9,17 @@ import { ScheduleListComponent } from './schedules/schedule-list/schedule-list.c
 // Define routes
 const routes: Routes = [
   { path: '', component: ScheduleListComponent },
-  { path: 'create', component: ScheduleCreateComponent},
-  { path: 'edit/:scheduleId', component: ScheduleCreateComponent },
+  // Guard these two paths
+  // Redirect an unauth to 'login' user that tries to manually enter these routes
+  { path: 'create', component: ScheduleCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:scheduleId', component: ScheduleCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
