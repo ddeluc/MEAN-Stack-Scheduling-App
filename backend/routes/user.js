aee4a13ca534733f2309c316ca1d56a19b589dde
ddeluc <56974496+ddeluc@users.noreply.github.com>
2020-12-05 01:35:45 -0500

    Retriving Token from Valid Login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/user");
+const user = require('../models/user');
 
 const router = express.Router();
 
-// Hash the password
+// Create a new user
 router.post("/signup", (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
@@ -26,6 +28,46 @@ router.post("/signup", (req, res, next) => {
           });
         });
     });
+});
+
+router.post('/login', (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email }).then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed."
+        });
+      }
+      // Compare the user password to the one in the database that matches the email
+      // Returns a Promise, result will be true or false
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      // Password does not match
+      if (!result) {
+        // Return is used to end execution
+        return res.status(401).json({
+          message: "Auth failed."
+        });
+      }
+      // Creates new web token (payload, secret, options)
+      // Will expire after 1 hour
+      const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
+        'example_secret_for_development_purpose',
+        {expiresIn: '1h'}
+      );
+      // Return token
+      res.status(200).json({
+        token: token
+      });
+    })
+    // Errpr
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed."
+      })
+    })
 });
 
 module.exports = router;
