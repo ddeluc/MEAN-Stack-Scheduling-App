b58b9f951cd401ac44d6992bdfb00c66738dbdc6
ddeluc <56974496+ddeluc@users.noreply.github.com>
2020-12-05 03:21:24 -0500

    Timeout Implementation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string | undefined;
   private authStatusListener = new Subject<boolean>();
+  private tokenTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -43,11 +44,16 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post('http://localhost:3000/api/user/login', authData)
+    this.http.post<{ token: string, expiresIn: number }>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          // Logout after an hour
+          setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000)
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           // Navigate to the homepage
@@ -61,5 +67,7 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     this.router.navigate(['/']);
+    // Clear timeout timer once logged out
+    clearTimeout(this.tokenTimer);
   }
 }
